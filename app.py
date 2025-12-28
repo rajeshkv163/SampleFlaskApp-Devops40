301ec9f3d608e9ff61dd5ccd26db94ec212dc742
sudhanshuvlog <pandeysudhanshuup@gmail.com>
2025-12-28 13:15:04 +0530

    fixed app

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask
 
 
 def create_app():
-    x=10
-    y=50
     app = Flask(__name__)
     print("inside create_app function")
 
@@ -14,16 +12,18 @@ def create_app():
 
     @app.route('/test')
     def test():
-        x=20
         return "test 1123456789 abcdefg79239279273927393923982938239239"
 
     @app.route('/test2')
     def test1():
-        return "test 20"
+        return "test 2"
 
     @app.route('/test3')
-    def test3():
+    def test2():
         return "test 1234"
+
+    return app
+
 
 if __name__ == '__main__':
     app = create_app()
